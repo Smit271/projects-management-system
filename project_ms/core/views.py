8f0cb8e7bcd4279e7c052c7057833dfdc43cfd73
Smit271 <smitpanchal32@gmail.com>
2022-02-05 19:12:51 +0530

    initial-step-done

---
+++ project_ms/core/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def dashboard(request):
+    return render(request, "core/dashboard.html")
+    
