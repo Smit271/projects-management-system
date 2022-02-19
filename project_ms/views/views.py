52a8f6fa6c55d5883fe1a98bbbabfaae9fab8815
Smit271 <smitpanchal32@gmail.com>
2022-02-19 19:30:52 +0530

    added-my-view

--- project_ms/views/views.py
+++ project_ms/views/views.py
@@ -1,6 +1,8 @@
+from multiprocessing import context
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from projects.models import Project
+from projects.models import Project, Task
+from user.models import User
 
 # Create your views here.
 # def index(request):
@@ -21,4 +23,11 @@ def project_view(request):
 
 def users_view(request):
     print("Views User")
-    return render(request, "views/index.html")
+    my_projects = Project.objects.filter(assign = request.user.id).order_by("id")
+    my_tasks = Task.objects.filter(assign = request.user.id).order_by("id")
+
+    context = {
+        'projects' : my_projects,
+        'tasks' : my_tasks,
+    }
+    return render(request, "views/user-view.html", context)
